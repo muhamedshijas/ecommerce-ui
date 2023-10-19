e4332cff102a9cf2a3b8e8dc21c04b1bdf2b663f
muhamedshijas <shijushijas157@gmail.com>
2023-10-19 18:24:25 +0530

    ui updated

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var sidebarBtn = document.getElementById("sidebarBtn");
-          var sidebar = document.getElementById("sidebar");
-          var sidebarContainer = document.getElementById("sidebarContainer");
-          var sideClose = document.getElementById("sideClose");
-      
-          sidebarBtn.addEventListener("click", ()=>{
-              sidebar.classList.add("open")
-          })
-          sidebar.addEventListener("click", ()=>{
-              sidebar.classList.remove("open")
-          })
-          sideClose.addEventListener("click", (e)=>{
-              e.stopPropagation()
-              sidebar.classList.remove("open")
-          })
-          sidebarContainer.addEventListener("click", (e)=>{
-              e.stopPropagation()
-              sidebar.classList.add("open")
-          })
+var sidebar = document.getElementById("sidebar");
+var sidebarContainer = document.getElementById("sidebarContainer");
+var sideClose = document.getElementById("sideClose");
+
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.add("open")
+})
+sidebar.addEventListener("click", () => {
+    sidebar.classList.remove("open")
+})
+sideClose.addEventListener("click", (e) => {
+    e.stopPropagation()
+    sidebar.classList.remove("open")
+})
+sidebarContainer.addEventListener("click", (e) => {
+    e.stopPropagation()
+    sidebar.classList.add("open")
+})
